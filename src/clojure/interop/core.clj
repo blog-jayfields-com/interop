d762764aa7c66224c2f9636f56f3c0ab709bb43d
Jay Fields <git@jayfields.com>
2011-12-24 09:48:03 -0500

    ctor example

--- src/clojure/interop/core.clj
+++ src/clojure/interop/core.clj
@@ -1,12 +1,4 @@
 (ns interop.core)
 
 (defn print-string [arg]
-  ;;; calling a static method
-  (println (String/valueOf true))
-
-  ;;; using an enum
-  (println (java.util.concurrent.TimeUnit/SECONDS))
-
-  ;;; using a Java nested (inner) class. Note, in Clojure you
-  ;;; use a $ instead of a .
-  (println (java.util.AbstractMap$SimpleEntry. "key" "val")))
+  (println (String. arg)))

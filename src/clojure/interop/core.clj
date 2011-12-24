ea5a526222da3046c6134bd3a4fb7d62392479f7
Jay Fields <git@jayfields.com>
2011-12-24 10:03:03 -0500

    create java objs that take java interfaces with clojure objs

--- src/clojure/interop/core.clj
+++ src/clojure/interop/core.clj
@@ -1,4 +1,11 @@
 (ns interop.core)
 
 (defn print-string [arg]
-  (println (String. arg)))
+  ;;; pass a Clojure vector where Java expects a java.util.Collection
+  (println (java.util.HashSet. ["1" "2"]))
+
+  ;;; pass a Clojure map where Java expects a java.util.Map
+  (println (java.util.LinkedHashMap. {1 "1" 2 "2"}))
+
+  ;;; pass a Clojure function where Java expects a Runnable
+  (println (Thread. (fn [] (println "clojure fns are runnables (and callables)")))))

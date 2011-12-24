2a56ba343c7365d48478b0281e7d3694011105f5
Jay Fields <git@jayfields.com>
2011-12-24 09:19:20 -0500

    static, enum, & nested class example

--- src/clojure/interop/core.clj
+++ src/clojure/interop/core.clj
@@ -1,4 +1,12 @@
 (ns interop.core)
 
 (defn print-string [arg]
-  (println (.replace arg "hello" "goodbye")))
+  ;;; calling a static method
+  (println (String/valueOf true))
+
+  ;;; using an enum
+  (println (java.util.concurrent.TimeUnit/SECONDS))
+
+  ;;; using a Java nested (inner) class. Note, in Clojure you
+  ;;; use a $ instead of a .
+  (println (java.util.AbstractMap$SimpleEntry. "key" "val")))
